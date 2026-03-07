2ce027b7af20b28350ead7930dfa2858d01d91f3
Yatichapat <yatichapat.d@ku.th>
2026-03-07 16:20:50 +0700

    backend(fix): add admin site register

--- backend/music/admin.py
+++ backend/music/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from music.models import EndUser, Song
+from music.models import *
 
 # Register your models here.
 admin.site.register(EndUser)
 admin.site.register(Song)
+admin.site.register(Album)
+admin.site.register(SharedLink)
+admin.site.register(Invitation)
